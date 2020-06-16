126487552a82268012a42a8a0c0fb9445c1cddee
lunpq <lunpq@10.0.0.101>
2020-06-16 00:23:14 -0300

    Correção do método post.

--- clinica/src/main/java/com/generation/clinica/controller/MedicoController.java
+++ clinica/src/main/java/com/generation/clinica/controller/MedicoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -54,7 +55,7 @@ import com.generation.clinica.repository.MedicoRepository;
 		
 		//put
 		
-		@PostMapping
+		@PutMapping
 		public ResponseEntity <MedicoModel> Put
 		(@RequestBody MedicoModel medico) {
 			return ResponseEntity.status(HttpStatus.OK).body(repository.save(medico));
--- clinica/src/main/java/com/generation/clinica/controller/PacienteController.java
+++ clinica/src/main/java/com/generation/clinica/controller/PacienteController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,7 +54,7 @@ public class PacienteController {
 	
 	//put
 	
-	@PostMapping
+	@PutMapping
 	public ResponseEntity <PacienteModel> Put
 	(@RequestBody PacienteModel paciente) {
 		return ResponseEntity.status(HttpStatus.OK).body(repository.save(paciente));
